2a81557fd053f1ed179b9b292ff4324beafe8e5c
blackawa <3104kurokawa@gmail.com>
2016-12-11 16:32:56 +0900

    :shower: remove unused .clj file

--- src/gt1gv1/users.clj
+++
@@ -1,7 +0,0 @@
-(ns gt1gv1.repository.users
-  (:require [clojure.java.jdbc :as j]
-            [honeysql.core :as sql]
-            [honeysql.helpers :refer :all]))
-
-(defn insert-users [user db]
-  (j/insert! :users user))
